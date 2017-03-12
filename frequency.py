1b30ac844d0d26bd501077d0d61ea41ad3fc11d3
Jeremy <jeremychristopherryan@gmail.com>
2017-03-12 16:25:14 -0400

    Created word frequency script.

--- frequency.py
+++ frequency.py
@@ -10,7 +10,15 @@ def get_word_list(file_name):
     returns a list of the words used in the book as a list.
     All words are converted to lower case.
     """
-    pass
+    f = open(file_name).read()
+    f = f[6200:-18820]
+    f = f.replace('--', ' ')
+    f = f.split()
+    index = 0
+    for item in f:
+        f[index] = item.strip(string.punctuation).lower()
+        index += 1
+    return f
 
 
 def get_top_n_words(word_list, n):
@@ -24,7 +32,23 @@ def get_top_n_words(word_list, n):
     frequently to least frequentlyoccurring
     """
     pass
+    f = word_list
+    d = {}
+    h = []
+    for word in word_list:
+        if word in d:
+            d[word] += 1
+        else:
+            d[word] = 1
+    for key in d:
+        h.append((d[key],key))
+    h.sort(reverse = True, key = lambda hist : hist[0])
+    return h[0:n]
 
 if __name__ == "__main__":
-    print("Running WordFrequency Toolbox")
-    print(string.punctuation)
+    mines = get_word_list('Solomon.txt')
+    mines = get_top_n_words(mines, 20)
+    for item in mines:
+        print(str(item[0]) + ": " + item[1])
+    #print("Running WordFrequency Toolbox")
+    #print(string.punctuation)
